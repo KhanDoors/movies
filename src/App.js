a349e755f47f857c62a7e88dc2d09e17909e6265
syyad <syyadkhan@yahoo.com>
2021-05-01 19:10:21 -0400

    basic Folder Structure set with RRD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import Header from "./components/Header";
+import MainNav from "./components/MainNav";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Movies from "./Pages/Movies";
+import Series from "./Pages/Series";
+import Trending from "./Pages/Trending";
+import Search from "./Pages/Search";
+import { Container } from "@material-ui/core";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+      <MainNav />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import "./Header.css";
+
+const Header = () => {
+  return <span className="header">🎞🎬 Movie App 📺📽 </span>;
+};
+
+export default Header;

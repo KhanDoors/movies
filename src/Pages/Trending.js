1038c07bf90419fb2e02afe016cf8358486b7edf
Syyad Khan <syykhan@deloitte.com>
2021-05-02 17:15:28 -0400

    fetchtrending

--- src/Pages/Trending.js
+++ src/Pages/Trending.js
@@ -1,6 +1,22 @@
 import "./Trending.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Trending = () => {
+  const [content, setContent] = useState([]);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_APIKEY}`
+    );
+    console.log(content);
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
   return <div className="trending">Trending</div>;
 };
 
